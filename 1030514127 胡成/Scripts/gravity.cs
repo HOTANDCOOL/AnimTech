8d793861c54ecd992bf7ab65aa0094c7fb2d2139
op1234567890 <1340394501@qq.com>
2017-04-10 09:26:03 +0800

    引力，阻力，振荡
    引力，阻力，振荡（基于牛顿力学的绘画笔刷）

---
+++ 1030514127 胡成/Scripts/gravity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravity : MonoBehaviour {
+
+	public float _强度=1.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rigidbody rd = GetComponent<Rigidbody>();
+		if (rd == null) {
+			return;
+		}
+		Vector3 localpos = transform.localPosition;
+		Vector3 force = -localpos.normalized * _强度 * 0.01f;
+		rd.AddForce (force);
+			
+
+	}
+}
---
+++ 1030514127 胡成/Scripts/resistance.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resistance : MonoBehaviour {
+
+
+	public float _阻力系数=0.1f;
+	public float mass = 1.0f;
+	private Vector3 acc;
+	private Vector3 v=new Vector3(0,0,0);
+	public float t = 0;
+	// Use this for initialization
+	void Start () {
+		
+		Vector3 F=new Vector3(1.0f,5.0f,0);
+		acc = F / mass;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float time = Time.realtimeSinceStartup;
+
+
+		Rigidbody rd = GetComponent<Rigidbody> ();
+
+		if (rd == null) {
+			return;
+		}
+
+		t += time;
+		//Vector3 LocalPos = transform.localPosition;
+
+
+		v += acc * time;
+		//Vector3 force = LocalPos-v;
+
+		rd.AddForce (-5*v.normalized);
+
+
+
+		if(t > 5.0f) {
+				acc -=v.normalized*_阻力系数 * v.magnitude* v.magnitude/ mass;
+		}
+
+	}
+}
